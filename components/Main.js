c42ce3929e38dba4cb8ff9f85dbd1dcb96cbc93a
Raphael Wirawan <alessandrowirawan@gmail.com>
2020-10-02 12:35:34 +0700

    working screen

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, TextInput } from "react-native";
 
-class Main extends Component {
+class Main extends React.Component {
+  state = { name: "" }; // 2. <- Add the component state
   render() {
-    return <View />;
+    return (
+      <View>
+        <TextInput
+          style={styles.nameInput}
+          placeHolder="John Cena"
+          value={this.state.name}
+        />
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({});
+const offset = 24;
+const styles = StyleSheet.create({
+  nameInput: {
+    // 3. <- Add a style for the input
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: "#111111",
+    borderWidth: 1,
+  },
+});
 
 export default Main;

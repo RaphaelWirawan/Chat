eb8dc2d4cd8ae4b89d9977c0aa5de5c0e469255b
Raphael Wirawan <alessandrowirawan@gmail.com>
2020-10-08 14:44:49 +0700

    -

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
-class Chat extends Component {
+class Chat extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || "Chat!",
+  });
+  state = {
+    messages: [],
+  };
+
   render() {
-    return <View />;
+    return <GiftedChat messages={this.state.messages} />;
   }
 }
-
-const styles = StyleSheet.create({});
-
 export default Chat;

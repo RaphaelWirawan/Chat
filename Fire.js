8ff970165fc7c3a9eaa7e254d9771700edbc866e
Raphael Wirawan <alessandrowirawan@gmail.com>
2020-10-09 23:04:58 +0700

    add Firebase key

--- Fire.js
+++ Fire.js
@@ -1,4 +1,37 @@
-class Fire {}
+import firebase from "firebase";
 
-Fire.shared = new Fire();
-export default Fire;
+class Fire {
+  constructor() {
+    this.init();
+
+    this.observeAuth();
+  }
+
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  onAuthStateChanged = (user) => {
+    if (!user) {
+      try {
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  init = () =>
+    firebase.initializeApp({
+      apiKey: "AIzaSyCYFRvwmcoGBfbQOo7jOfwNuoxtw0CFyfA",
+      authDomain: "chat-3ba97.firebaseapp.com",
+      databaseURL: "https://chat-3ba97.firebaseio.com",
+      projectId: "chat-3ba97",
+      storageBucket: "chat-3ba97.appspot.com",
+      messagingSenderId: "576325407209",
+      appId: "1:576325407209:web:b05855e437f78cdced7b78",
+      measurementId: "G-Y65LP53FFJ",
+    });
+}
+
+//Fire.shared = new Fire();
+//export default Fire;
